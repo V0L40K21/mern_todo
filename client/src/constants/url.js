e616b8727963659eb6df6c20272356f9f60ce4a4
Vitaliy <Kamenchshikov>
2021-09-16 23:36:34 +0600

    heroku

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = 'http://localhost:5000';
-// const BACKEND_URL = 'http://vol4ok.pp.ua';
+// const BACKEND_URL = 'http://localhost:5000';
+const BACKEND_URL = 'http://vol4ok.pp.ua';
 
 const ENDPOINT_AUTH = BACKEND_URL + '/api/auth/login';
 const ENDPOINT_REGISTER = BACKEND_URL + '/api/auth/register';
