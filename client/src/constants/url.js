43ade896246e1b6773f3b89ab7973f771de40a23
V0L40K <vol4ok94@gmail.com>
2021-09-18 01:10:02 +0600

    web is done

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = 'http://127.0.0.1:5000'
-// const BACKEND_URL = 'https://vol4ok.herokuapp.com'
+// const BACKEND_URL = 'http://127.0.0.1:5000'
+const BACKEND_URL = 'https://vol4ok.herokuapp.com'
 
 const ENDPOINT_AUTH = BACKEND_URL + '/api/auth/login'
 const ENDPOINT_REGISTER = BACKEND_URL + '/api/auth/register'
